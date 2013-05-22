b8d10e356558304987d550b762433a003728ad9a
Jon Welborn <smrk3r@gmail.com>
2013-05-22 15:05:10 -0400

    initial commit
    lol.

---
+++ vuln.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+
+	char buf[64];
+
+	if (argc < 2)
+	{
+		printf("syntax error\r\n");
+		printf("must supply at least one argument\r\n");
+		return(1);
+	{
+
+	strcopy(buf,argv[1]); 
+
+	return(0);
+}
